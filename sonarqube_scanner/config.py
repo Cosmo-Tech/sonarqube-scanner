87a7d56b839634b630fe13e97508a27f75b33395
Diamantis Sellis <diamantis.sellis@cosmotech.com>
2025-03-18 12:03:57 +0100

    fix: branches

--- sonarqube_scanner/config.py
+++ sonarqube_scanner/config.py
@@ -2,6 +2,7 @@
 Configuration handling for the SonarQube Scanner.
 """
 
+import os
 import yaml
 import logging
 logger = logging.getLogger("sonarqube_scanner")
@@ -10,6 +11,8 @@ def load_config(config_path):
     """Load and parse YAML configuration file."""
     try:
         with open(config_path) as f:
-            return yaml.safe_load(f)
+            config = yaml.safe_load(f)
+            config['sonarqube']['token'] = os.getenv('SONARQUBE_TOKEN')
+            return config
     except Exception as e:
         logger.error(f"Error reading configuration: {e}")
